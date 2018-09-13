b1456b2fad25667d6ba9bda34919ca321b4a4fe6
kasia brzozowicz <k.m.brzozowicz@gmail.com>
2018-09-13 23:29:30 +0200

    Setup themoviedb library and add api key

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import theMovieDb from "themoviedb-javascript-library";
+
+
+theMovieDb.common.api_key = process.env.REACT_APP_API_KEY;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
